9519f4ca356d5c2e49557d06e4e46084491487dd
LinuxHjkaru <hathetrungmanu@gmail.com>
2014-12-24 16:43:18 +0700

    Add User mode

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module SampleWeb
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   end
 end
